c3a5cc0bd57ac4475c394798b41d21fa88cc74d2
Paul-Nicolas Madelaine <pnm@pnm.tf>
2023-10-24 20:34:00 +0200

    add etna.typhon-ci.org

--- typhon-ci.org.py
+++ typhon-ci.org.py
@@ -4,6 +4,8 @@ from gandi import Domain
 
 gandi_ip_v4 = "92.243.26.156"
 gandi_ip_v6 = "2001:4b98:dc0:43:f816:3eff:fe36:b573"
+ovh_ip_v4 = "51.178.183.169"
+ovh_ip_v6 = "2001:41d0:404:200::38e2"
 
 typhon = Domain("typhon-ci.org")
 
@@ -11,5 +13,8 @@ typhon.a("@", gandi_ip_v4)
 typhon.a("www", gandi_ip_v4)
 typhon.aaaa("@", gandi_ip_v6)
 typhon.aaaa("www", gandi_ip_v6)
+
+typhon.a("etna", ovh_ip_v4)
+typhon.aaaa("etna", ovh_ip_v6)
 
 print(typhon.push())
