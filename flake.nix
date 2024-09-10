99dab8fd0f669fddfbb6877bd62e2fcb13a3823a
Paul-Nicolas Madelaine <pnm@pnm.tf>
2024-09-10 18:27:10 +0200

    format using nixfmt-rfc-style

--- flake.nix
+++ flake.nix
@@ -4,19 +4,29 @@
     flake-utils.url = "flake-utils";
   };
 
-  outputs = {
-    self,
-    nixpkgs,
-    flake-utils,
-  }:
-    flake-utils.lib.eachDefaultSystem (system: let
-      pkgs = import nixpkgs {inherit system;};
-    in {
-      devShells.default = pkgs.mkShell {
-        packages = [
-          (pkgs.python3.withPackages
-            (ps: with ps; [certifi httplib2 json5]))
-        ];
-      };
-    });
+  outputs =
+    {
+      self,
+      nixpkgs,
+      flake-utils,
+    }:
+    flake-utils.lib.eachDefaultSystem (
+      system:
+      let
+        pkgs = import nixpkgs { inherit system; };
+      in
+      {
+        devShells.default = pkgs.mkShell {
+          packages = [
+            (pkgs.python3.withPackages (
+              ps: with ps; [
+                certifi
+                httplib2
+                json5
+              ]
+            ))
+          ];
+        };
+      }
+    );
 }
