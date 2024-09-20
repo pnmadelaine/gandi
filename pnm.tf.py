8b4b8cad50403ec3305f1a5251969281310cbe03
Paul-Nicolas Madelaine <pnm@pnm.tf>
2024-09-20 17:52:18 +0200

    update mailo config

--- pnm.tf.py
+++ pnm.tf.py
@@ -8,13 +8,11 @@ root = Domain("pnm.tf")
 root.txt("@", "google-site-verification=qE-tHUdg2oCWilirbY7HeYq_kRVR1s3Pb1ujq6D0y5Q")
 root.cname("b6cf11c9ba9ca8a6b661ffb0d0b99f23", "verify.bing.com.")
 
-# DMARC for all mails
-root.txt("_dmarc", "v=DMARC1; p=none; rua=mailto:root@pnm.tf")
-
 # mailo
 root.txt("@", "mailo=giJHSYjeyz3bCIAriAxVjcGHgOrxjcon")
 root.mx("@", "mx.mailo.com.")
-root.spf("@", "a include:mailo.com")
+root.spf("@", "v=spf1 a include:mailo.com -all")
+root.txt("_dmarc", "v=DMARC1; p=quarantine;")
 
 # github
 root.txt("_github-pages-challenge-pnmadelaine", "299d6a2706fcb22d5aa713aaa4472c")
